6acabc9f53787fcc46fabad93d6d17197845a9bf
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-08-31 18:11:28 +0200

    Define a basic GameBoard component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import GameBoard from './components/GameBoard'
+
 function App() {
 	return (
 		<div className="App">
@@ -8,7 +10,9 @@ function App() {
 				React Breakout
 			</header>
 			<section className="App-body">
-				<div className="game-container"></div>
+				<div className="game-container">
+					<GameBoard />
+				</div>
 				<aside className="side-panel-container"></aside>
 			</section>
 		</div>
