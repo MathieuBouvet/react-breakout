c408a3169313c65f45b27341350a12ba734a02b0
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-08-31 18:36:43 +0200

    Implement a basic paddle movement

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
-
 import './GameBoard.css'
 
+import Paddle from './Paddle'
+
 class GameBoard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			paddlePosition: 0,
+		}
+	}
 
 	render(){
+		const { paddlePosition } = this.state;
 		return (
-			<div className="gameBoard"> game </div>
+			<div className="gameBoard" onMouseMove={this.handleMouseMove}>
+				<Paddle leftPosition={paddlePosition} />
+			</div>
 		)
+	}
+
+	handleMouseMove = (event) => {
+		this.setState({
+			paddlePosition: event.nativeEvent.offsetX,
+		});
 	}
 }
 
