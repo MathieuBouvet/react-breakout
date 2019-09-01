12f6f805c4f7b50ea70d852d21f824e181b0f41e
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-01 23:53:49 +0200

    Add collide method in PaddleModel to handle ths collission with the ball

--- src/model/PaddleModel.js
+++ src/model/PaddleModel.js
@@ -4,6 +4,16 @@ class PaddleModel extends Box{
 	constructor(reduction) {
 		super("paddle", reduction);
 	}
+
+	collide(ball){
+		const next = ball.getNextPosition();
+		// ratio of the collision offset
+		const ballOffset = (next.left - this.leftPosition) / this.width;
+
+		// projection of this ratio between 135 (max left angle) and 45 (max right angle)
+		const angle = 135 - 90 * ballOffset;
+		ball.moveTo(angle);
+	}
 }
 
 export default PaddleModel
