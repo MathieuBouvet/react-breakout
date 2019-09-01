38746b2895184fda0b678d0a7281f5d8de7f4dd5
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-01 18:19:56 +0200

    Use the ConfigurableEntity as a parent for PaddleModel

--- src/model/PaddleModel.js
+++ src/model/PaddleModel.js
@@ -1,8 +1,12 @@
-import Settable from './Settable'
+import ConfigurableEntity from './ConfigurableEntity'
 
-class PaddleModel extends Settable{
+class PaddleModel extends ConfigurableEntity{
 	constructor(reduction) {
 		super("paddle", reduction);
+		this.setting("width");
+		this.setting("height");
+		this.setting("topPosition");
+		this.setting("leftPosition");
 	}
 }
 
