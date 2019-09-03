c1a5256d96aecf1750d17fe6ed221d70079fc790
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-03 15:47:01 +0200

    Implement collisionHow for a Brick

--- src/model/BrickModel.js
+++ src/model/BrickModel.js
@@ -6,6 +6,22 @@ class BrickModel extends Box {
 		this.leftPosition = 200;
 		this.topPosition = 200;
 	}
+
+	collisionHow(ball){
+
+		// Will hit horizontal border ?
+		if(ball.topPosition < this.topPosition || ball.topPosition > this.topPosition+this.height){
+			// will hit a corner ?
+			if(ball.leftPosition < this.leftPosition || ball.leftPosition > this.leftPosition+this.width){
+				// corner
+				return 45;
+			}
+			// horizontal
+			return 0;
+		}
+		// vertical
+		return 90;
+	}
 }
 
 export default BrickModel
