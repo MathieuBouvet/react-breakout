dab3ec3b20022e9aa7de8b073beeaf223617a7f1
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-11 19:56:09 +0200

    Create the Dialog component

---
+++ src/components/Dialog.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Dialog.css'
+
+const Dialog = (props) => (
+	<div className="dialog-overlay">
+		<div className="dialog">
+			test
+		</div>
+	</div>
+)
+
+Dialog.propTypes = {
+
+}
+
+export default Dialog
