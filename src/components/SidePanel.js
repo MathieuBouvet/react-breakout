480ce37b703290fdd00f18fb3013552906807792
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-11 17:05:32 +0200

    Create SidePanel component

---
+++ src/components/SidePanel.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './SidePanel.css'
+
+const SidePanel = (props) => (
+	<aside className="side-panel-container">
+	</aside>
+)
+
+SidePanel.propTypes = {
+
+}
+
+export default SidePanel
