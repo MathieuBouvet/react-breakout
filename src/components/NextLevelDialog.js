8405de630bedcf1b7e8e453d47b33e54f2489055
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-13 17:52:29 +0200

    Pass context to define icon class and style the component

--- src/components/NextLevelDialog.js
+++ src/components/NextLevelDialog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FaArrowCircleRight } from 'react-icons/fa'
+import { IconContext } from 'react-icons'
 
 import './NextLevelDialog.css'
 
@@ -8,7 +9,10 @@ import Dialog from './Dialog'
 
 const NextLevelDialog = (props) => (
 	<Dialog title="Gagné !">
-	<FaArrowCircleRight />
+		<IconContext.Provider value={{ className: "next-level-icon" }}>
+			<FaArrowCircleRight />
+		</IconContext.Provider>
+		<div className="next-level-label">Suivant</div>
 	</Dialog>
 )
 
