b49671663826177bf24ab49fe68978a62771a2d5
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-21 11:21:44 +0200

    Create a basic GameWonDialog component

---
+++ src/components/GameWonDialog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Dialog from './Dialog'
+
+import './GameWonDialog.css'
+
+const GameWonDialog = (props) => (
+	<Dialog title="Jeu Terminé !">
+		c'est gagné !
+	</Dialog>
+)
+
+export default GameWonDialog
