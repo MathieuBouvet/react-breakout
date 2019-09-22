cca769562f684a4e4435c969dfd6a6aa1c6b5f96
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-22 16:36:55 +0200

    Pass a prop to Sizable to control the padding of the sizable

--- src/components/Sizable.js
+++ src/components/Sizable.js
@@ -3,21 +3,27 @@ import PropTypes from "prop-types";
 
 import "./Sizable.css";
 
-const Sizable = ({ width, height, children }) => (
+const Sizable = ({ width, height, margin, children }) => (
 	<div
 		className="sizable"
 		style={{
 			height: height + "px",
 			width: width + "px",
+			padding: margin + "px",
 		}}
 	>
 		{children}
 	</div>
 );
 
+Sizable.defaultProps = {
+	margin: 5,
+};
+
 Sizable.propTypes = {
 	width: PropTypes.number.isRequired,
 	height: PropTypes.number.isRequired,
+	margin: PropTypes.number,
 };
 
 export default Sizable;
