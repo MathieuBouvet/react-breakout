84a218b21567f10152d3ffe2ffb837c759475534
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-22 16:29:20 +0200

    Format Sizable component using prettier

--- src/components/Sizable.js
+++ src/components/Sizable.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import './Sizable.css'
+import "./Sizable.css";
 
-const Sizable = ({width, height, children}) => (
-	<div 
+const Sizable = ({ width, height, children }) => (
+	<div
 		className="sizable"
 		style={{
-			height: height+'px',
-			width: width+'px',
-		}}>
+			height: height + "px",
+			width: width + "px",
+		}}
+	>
 		{children}
 	</div>
-)
+);
 
 Sizable.propTypes = {
 	width: PropTypes.number.isRequired,
 	height: PropTypes.number.isRequired,
-}
+};
 
-export default Sizable
+export default Sizable;
