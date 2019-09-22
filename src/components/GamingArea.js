3c84280e3a679a40bf44162a7d242500de47fd80
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-22 16:53:23 +0200

    Format GamingArea component using prettier

--- src/components/GamingArea.js
+++ src/components/GamingArea.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import { BrickTypes } from '../model/Settings'
+import { BrickTypes } from "../model/Settings";
 
-import Paddle from './Paddle'
-import Ball from './Ball'
-import Brick from './Brick'
+import Paddle from "./Paddle";
+import Ball from "./Ball";
+import Brick from "./Brick";
 
-import './GamingArea.css'
+import "./GamingArea.css";
 
-const GamingArea = ({gameModelSize, paddle, ball, bricks, mouseMoveHandler, clickHandler}) => (
-	<div className="gameBoard" style={{width: gameModelSize.width+"px", height: gameModelSize.height+"px"}}>
+const GamingArea = ({ gameModelSize, paddle, ball, bricks, mouseMoveHandler, clickHandler }) => (
+	<div className="gameBoard" style={{ width: gameModelSize.width + "px", height: gameModelSize.height + "px" }}>
 		<div className="mouseMoveOverlay" onMouseMove={(e) => mouseMoveHandler(e)} onClick={() => clickHandler()}></div>
 		<Paddle {...paddle} />
 		<Ball {...ball} />
-		{ bricks.map((brick) => (
-			<Brick 
+		{bricks.map((brick) => (
+			<Brick
 				key={brick.id}
 				top={brick.topPosition}
 				left={brick.leftPosition}
@@ -23,12 +23,11 @@ const GamingArea = ({gameModelSize, paddle, ball, bricks, mouseMoveHandler, clic
 				height={brick.height}
 				display={!brick.markForRemove}
 				type={brick.type}
-				life={brick.type===BrickTypes.UNBREAKABLE ? -1 : brick.life}
+				life={brick.type === BrickTypes.UNBREAKABLE ? -1 : brick.life}
 			/>
-		)) }
-		
+		))}
 	</div>
-)
+);
 
 GamingArea.propTypes = {
 	gameModelSize: PropTypes.shape({
@@ -46,16 +45,18 @@ GamingArea.propTypes = {
 		left: PropTypes.number.isRequired,
 		size: PropTypes.number.isRequired,
 	}).isRequired,
-	bricks: PropTypes.arrayOf(PropTypes.shape({
-		topPosition: PropTypes.number.isRequired,
-		leftPosition: PropTypes.number.isRequired,
-		width: PropTypes.number.isRequired,
-		height: PropTypes.number.isRequired,
-		markForRemove: PropTypes.bool.isRequired,
-		type: PropTypes.number.isRequired,
-	})).isRequired,
+	bricks: PropTypes.arrayOf(
+		PropTypes.shape({
+			topPosition: PropTypes.number.isRequired,
+			leftPosition: PropTypes.number.isRequired,
+			width: PropTypes.number.isRequired,
+			height: PropTypes.number.isRequired,
+			markForRemove: PropTypes.bool.isRequired,
+			type: PropTypes.number.isRequired,
+		})
+	).isRequired,
 	mouseMoveHandler: PropTypes.func.isRequired,
 	clickHandler: PropTypes.func.isRequired,
-}
+};
 
-export default GamingArea
+export default GamingArea;
