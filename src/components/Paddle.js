ab9f245905140771bc23c2e017c403418cad6d7f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-22 16:44:07 +0200

    Format Paddle component using prettier

--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import './Paddle.css'
+import "./Paddle.css";
 
-import Positionable from './Positionable'
-import Sizable from './Sizable'
+import Positionable from "./Positionable";
+import Sizable from "./Sizable";
 
-const Paddle = ({top, left, width, height}) => (
+const Paddle = ({ top, left, width, height }) => (
 	<Positionable top={top} left={left}>
 		<Sizable width={width} height={height}>
 			<div className="paddle"></div>
 		</Sizable>
 	</Positionable>
-)
+);
 
 Paddle.propTypes = {
 	top: PropTypes.number.isRequired,
 	left: PropTypes.number.isRequired,
 	width: PropTypes.number.isRequired,
 	height: PropTypes.number.isRequired,
-}
+};
 
-export default Paddle
+export default Paddle;
