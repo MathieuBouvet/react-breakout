c341db00c6a2e0e86ad382363d647e0881c57ea8
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-09-22 11:24:47 +0200

    Format Dialog component with prettier

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import './Dialog.css'
+import "./Dialog.css";
 
-const Dialog = ({title, dialogAction, children}) => (
+const Dialog = ({ title, dialogAction, children }) => (
 	<div className="dialog-overlay">
 		<div className="dialog">
-			<div className="dialog-header">{ title }</div>
+			<div className="dialog-header">{title}</div>
 			<div className="dialog-body" onClick={() => dialogAction()}>
-				{ children }
+				{children}
 			</div>
 		</div>
 	</div>
-)
+);
 
 Dialog.propTypes = {
 	title: PropTypes.string.isRequired,
 	dialogAction: PropTypes.func.isRequired,
-}
+};
 
-export default Dialog
+export default Dialog;
